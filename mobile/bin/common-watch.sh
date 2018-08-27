45b57c89db7080168ca9c31b35a4b3bb80a62449
Kyle Johnson <kyle@solidstategroup.com>
2018-08-27 16:34:19 +0100

    Improve common sync

--- mobile/bin/common-watch.sh
+++ mobile/bin/common-watch.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-pidfile=./file-watch.pid
+LINE=$(ps aux | grep -w "common-watch.sh" | grep -v grep | wc -l)
+echo $LINE
 
-if [[ $1 ]]; then
-    rm $pidfile
-fi
-
-# If the pid file exists we must already be running
-if ! ln -s "pid=$$" "$pidfile"; then
-  echo "Already running. Exiting." >&2
-  exit 0
-fi
-
- # remove pid if we exit normally or are terminated
-  trap "rm -f $pidfile" 0 1 3 15
+if [ "$LINE" -le "3" ]
+then
   npm install -g wml
   watchman watch ../../common
   wml add ../../common ../common-mobile
   wml start
+ else
+     echo "Process already running"
+     exit
+
+fi
